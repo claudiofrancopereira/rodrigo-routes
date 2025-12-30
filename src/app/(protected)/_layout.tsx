46f87091f413ef24b09e5dbb4133194b89eca013
Claudio Pereira <claudiofrancopereira@gmail.com>
2025-12-30 15:36:23 -0300

    Second commit

---
+++ src/app/(protected)/_layout.tsx
@@ -0,0 +1,46 @@
+import { Redirect, Stack } from "expo-router";
+
+import { useAuth } from "@/hooks/useAuth";
+import { ActivityIndicator } from "react-native";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function ProtectedLayout() {
+    const { isLoggedIn, isReady } = useAuth()
+
+    if (!isReady) {
+        return (
+            <ActivityIndicator 
+                style={{ flex: 1, justifyContent: "center", alignItems: "center" }} 
+            
+            />
+
+        )
+
+    }
+    
+    if (!isLoggedIn) {
+        return <Redirect href="/signIn" />
+
+    }
+
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ title: "Home" }} />
+
+        </Stack>
+
+    )
+
+}
---
+++ src/app/signIn.tsx
@@ -0,0 +1,29 @@
+import { Button, View } from "react-native";
+
+import { useAuth } from "@/hooks/useAuth";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function SignIn() {
+    const { signIn } = useAuth()
+
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Button title="Entrar" onPress={signIn} />
+
+        </View>
+
+    )
+
+}
